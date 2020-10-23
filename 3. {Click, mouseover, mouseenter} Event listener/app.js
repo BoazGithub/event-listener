2842fc0c82c7d2b6b3cc7e6ba2bb555c71597a1a
BoazGithub <65084796+BoazGithub@users.noreply.github.com>
2020-10-23 16:25:11 +0200

    Add files via upload
    This mouse event listener that will manipulate a DOM at the time user commit it.

---
+++ 3. {Click, mouseover, mouseenter} Event listener/app.js
@@ -0,0 +1,19 @@
+
+
+var button = document.getElementById("button1");
+button1.addEventListener("click", function(){
+	document.body.style.backgroundColor="tomato";
+});
+
+var button = document.getElementById("button2");
+button2.addEventListener("mouseenter", function(){
+	document.body.style.backgroundColor="red";
+});
+var button = document.getElementById("button3");
+button3.addEventListener("mouseenter", function(){
+	document.body.style.backgroundColor="blue";
+});
+var button4 = document.getElementById("button4");
+button4.addEventListener("dblclick", function(){
+	document.body.style.backgroundColor="#f9f9f9";
+});
